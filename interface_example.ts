5bc46890ffadae6dcda82566b8441872e6f9f320
Savita05 <33056677+Savita05@users.noreply.github.com>
2021-04-14 09:44:04 +0530

    Add files via upload

---
+++ interface_example.ts
@@ -0,0 +1,10 @@
+interface OS {
+    name:String;
+    language: String;  
+}  
+let OperatingSystem = (type: OS): void => {  
+  console.log('Android ' + type.name + ' has ' + type.language + ' language.');  
+};  
+let Oreo = {name: 'O', language: 'Java'}  
+OperatingSystem(Oreo);
+
